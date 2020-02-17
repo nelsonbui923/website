c456ebda6ffd281e390c243022b464078ce990a1
Nelson Bui <nelson.bui923@gmail.com>
2020-02-17 00:48:08 -0800

    NEW UPDATED WEBSITE??

--- script.js
+++ script.js
@@ -1,90 +1,115 @@
+
 const contactLink = document.querySelector(".navContact");
 const homeLink = document.querySelector(".navHome");
 const portfolioLink = document.querySelector(".navPortfolio");
+const navButton = document.querySelector('button[aria-expanded]');
+const nav = document.querySelector('#main');
+// const submitButton = document.querySelector('.contactButton');
+// const modalOuter = document.querySelector('.modal-outer');
+// const modalInner = document.querySelector('.modal-inner');
+const sliderImages = document.querySelectorAll('.slide-in');
+let topOfNav = nav.offsetTop;
 
+navButton.addEventListener('click', toggleNav);
+window.addEventListener('scroll', fixNav);
+// ALL MY ONLCICK FUNCTIONS GO HERE
 
-
-contactLink.onclick = function redirectContact() {
-  console.log("Redirecting to contact page..");
-  window.location.href = "contact.html";
+function redirectContact() {
+    window.location.href = "contact.html";
 }
 
-homeLink.onclick = function redirectHome() {
-  console.log("Redirecting to home page..");
-  window.location.href = "index.html";
+function redirectHome() {
+    window.location.href = "index.html";
 }
 
-portfolioLink.onclick = function redirectPortfolio() {
-  console.log("Redirecting to portfolio page..");
-  window.location.href = "portfolio.html";
+function redirectPortfolio() {
+    window.location.href = "index.html#portfolio";
 }
 
-const myImage = document.querySelector("#selfie");
+function redirectAboutMe() {
+    window.location.href = "portfolio.html";
+}
 
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/s960x960/79263664_2550614018309492_7361371308786974720_o.jpg?_nc_cat=111&_nc_ohc=uoUiwQ8J0wEAQlq2KH8P_v_CsldwNJHRBOe9LSel3wERHFsYiXUZbQLgA&_nc_ht=scontent-lax3-2.xx&oh=9a763ed890b252e4d61396495900fe55&oe=5E6E4CB2") {
-    myImage.setAttribute('src', "https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/882702_3812400807521_199069633_o.jpg?_nc_cat=107&_nc_ohc=5ylVXgLXhyUAQkL7I0fghtSLz6ulHQq5gkY5mDLym7IK43ApO1ZJT5Z9Q&_nc_ht=scontent-lax3-2.xx&oh=60f77a7abdc6707dbb25b9f24aa415ea&oe=5E6ABC71")
+function redirectBlackjack() {
+  window.location.href = "https://nelson-blackjack.netlify.com/"
+}
+
+// // NAV BAR
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
   } else {
-    myImage.setAttribute('src', "https://steamuserimages-a.akamaihd.net/ugc/951848487721743577/E7448773225D209DA33857EFD72B668EF6435918/")
-  };
-  console.log("Picture switch!")
-};
-
-// const instagramLink = document.getElementById('instagram');
-// const linkedinLink = document.getElementById('linkedin');
-// instagramLink.onclick = function redirectInsta() {
-//   window.open('https://www.instagram.com/the_real_nelsonbui/')
-// }
-function instaLink() {
-  window.open('https://www.instagram.com/the_real_nelsonbui/')
-}
-
-function linkedinLink () {
-  window.open('https://www.linkedin.com/in/nelson-bui/')
-}
-
-
-function submitMe() {
-  console.log( 'Sending data' );
-  const form = document.forms["contactForm"];
-  let nameInput = form["name"].value;
-  let emailInput = form["email"].value;
-  let messageInput = form["message"].value;
-console.log(`Name: ${nameInput}, Email: ${emailInput}, Message: ${messageInput}`);
-  if (typeof nameInput === "string" && typeof emailInput === "string" && typeof messageInput === "string") {
-    alert(`Thank you for visiting, ${nameInput}!`)
-  } else {
-    alert("Please enter your name, email, and message.")
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
   }
-  window.open('./thankyou.html')
 }
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
+
+
+// SANDWICH FOR PHONE VIEW
+
+function toggleNav({ target }) {
+  const expanded = target.getAttribute('aria-expanded') === 'true' || false;
+  navButton.setAttribute('aria-expanded', !expanded);
+}
+
+navButton.addEventListener('click', toggleNav);
+
+// ABOUT ME SCROLLING IMAGES
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+      // half way through the image
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+      // bottom of the image
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active');
+      } else {
+        sliderImage.classList.remove('active');
+      }
+    });
+    }
+
+// function handleSubmit() {
+//   modalOuter.classList.add('open');
 // }
-//
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
+
+// function closeModal() {
+//   modalOuter.classList.remove('open');
 // }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
+
+// modalOuter.addEventListener('click', function(event) {
+//   const isOutside = !event.target.closest('.modal-inner');
+//   if (isOutside) {
+//     closeModal();
 //   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+// });
+
+
+
+navButton.addEventListener('click', toggleNav);
+window.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', debounce(checkSlide));
+// submitButton.addEventListener('click', handleSubmit)
